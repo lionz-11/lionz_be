e831d091238cecf5fa1722fbcd1f6dab85f4eec9
hanju <97gkswn@gmail.com>
2023-03-15 03:17:58 +0900

    전역jwt설정

--- src/main/java/haja/Project/config/SwaggerConfig.java
+++ src/main/java/haja/Project/config/SwaggerConfig.java
@@ -1,9 +1,74 @@
 package haja.Project.config;
 
+
+
+
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
+
+
 
 @Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
 
+        Info info = new Info()
+                .version("v1.0.0")
+                .title("API 타이틀")
+                .description("API Description");
+
+        // SecuritySecheme명
+        String jwtSchemeName = "jwtAuth";
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    @Bean
+    public OperationCustomizer operationCustomizer() {
+        return (Operation operation, HandlerMethod handlerMethod) -> {
+            Parameter param = new Parameter()
+                    .in(ParameterIn.HEADER.toString())  // 전역 헤더 설정
+                    .schema(new StringSchema()._default("1234567").name("AppID")) // default값 설정
+                    .name("AppID")
+                    .description("TEST AppID")
+                    .required(true);
+            operation.addParametersItem(param);
+            return operation;
+        };
+    }
+
+//    @Bean
+//    public GroupedOpenApi group2(OperationCustomizer operationCustomizer) { // 파라메타 추가
+//        return GroupedOpenApi.builder()
+//                .group("그룹2")
+//                .pathsToMatch("/group2/**")
+//                .addOperationCustomizer(operationCustomizer)  // 전역 파라메타
+//                .build();
+//    }
 
 }
